e05b0099bbf575ddc1b35bb8a2de2c38564fcf22
Christopher Barnes <chris.barnes.2000@me.com>
2020-04-22 13:51:09 -0700

    [add] HW7 ?2 FindEvenDigitNumbers

--- HW7/Find_Even_Digit_Numbers.py
+++ HW7/Find_Even_Digit_Numbers.py
@@ -1,18 +1,27 @@
 """Problem: 
-    -
+    - Given an array nums of integers, return how many of them contain an even number of digits.
 
     Example Input:
-        -
+        -Input: nums = [12,345,2,6,7896]
 
     Example Output:
-        -
+        -Output: 2
+
+    Explanation: 
+    12 contains 2 digits (even number of digits). 
+    345 contains 3 digits (odd number of digits). 
+    2 contains 1 digit (odd number of digits). 
+    6 contains 1 digit (odd number of digits). 
+    7896 contains 4 digits (even number of digits). 
+    Therefore only 12 and 7896 contain an even number of digits.
 
 -1 Restate the problem
     -
 -2 Ask clarifying questions
     -
 -3 State your assumptions
-    -
+    - 1 <= nums.length <= 500
+    - 1 <= nums[i] <= 10^5
 -4 Think out loud
   -4a Brainstorm solutions
         -
@@ -34,9 +43,8 @@
         After implementing some code go back through and revaluate its time and/or space complexity -- refractor/improve/find more edge cases -- repeat
 """
 
-
 class Solution:
-    def findNumbers(self, nums: List[int]) -> int:
+    def findNumbers(self, nums: [int]) -> int:
         count = 0
         for _ in nums:
             if len(list(str(_))) % 2 == 0:
@@ -44,7 +52,7 @@ class Solution:
         return count
 
 if __name__ == "__main__":                              # RunTime:
-    findNumbers = Solution.findNumbers()
+    s = Solution()
     param = [12, 345, 2, 6, 7896]
-    function_output = findNumbers(param)
+    function_output = s.findNumbers(param)
     print(function_output)
